7f800cc903a69127e48fabf10fc26e69337471e1
’Belal’ <bshaheen@hamilton.edu>
2021-02-17 22:32:31 -0500

    Update Player.tsx

---
+++ client/src/components/Player.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Block } from "baseui/block";
+
+export const Player = () => {
+  return (
+    <Block
+      width="40%"
+      display="flex"
+      flexDirection="column"
+      text-align="center"
+      justifyContent="center"
+      margin="0 auto"
+      marginTop="12%"
+    ></Block>
+  );
+};
